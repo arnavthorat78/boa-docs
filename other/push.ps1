7b207f0edcdef0847087da85414da1ffba884d17
arnavthorat78 <arnavthorat78@gmail.com>
2022-02-20 12:35:38 +1100

    Added subtree command

--- other/push.ps1
+++ other/push.ps1
@@ -13,12 +13,12 @@ git commit -m $m
 git push
 
 Write-Host "Starting commiting process on gh-pages branch." -ForegroundColor green
-git checkout main
-git checkout source "dist/"
-git add .
-git commit -m $m
-git pull --rebase
-git push origin gh-pages
+git subtree push --prefix dist origin gh-pages
+# git checkout main
+# git checkout source "dist/"
+# git add .
+# git commit -m $m
+# git push origin gh-pages
 
 Write-Host "Ending processes." -ForegroundColor green
 git checkout main
