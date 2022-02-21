0ead947b3342e1c492a926b03ba2da01d795fc11
arnavthorat78 <arnavthorat78@gmail.com>
2022-02-21 17:17:34 +1100

    No message specified

--- dist/js/index.js
+++ dist/js/index.js
@@ -16,7 +16,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\r\nconsole.log(\"Hello, world!\");\r\n\n\n//# sourceURL=webpack://boa-docs/./src/index.ts?");
+eval("\r\n/**\r\n * The button that will be clicked to download the software.\r\n */\r\nconst downloadButton = document.querySelector(\".download-button\");\r\n/**\r\n * The element for the message to display to the user if their OS does not support the installer.\r\n */\r\nconst downloadMessage = document.querySelector(\".download-message\");\r\n/**\r\n * A boolean value which indicates if the user is on a `macOS` machine.\r\n */\r\nconst osIsMac = navigator.platform.toLowerCase().includes(\"mac\"); // TODO Change navigator.platform to a non-deprecated value\r\nif (!osIsMac) {\r\n    downloadButton.classList.add(\"disabled\");\r\n    downloadMessage.innerHTML = \"The installer can only be run on <strong>macOS</strong>.\";\r\n}\r\n\n\n//# sourceURL=webpack://boa-docs/./src/index.ts?");
 
 /***/ })
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, world!");
+/**
+ * The button that will be clicked to download the software.
+ */
+const downloadButton = document.querySelector(".download-button")!;
+/**
+ * The element for the message to display to the user if their OS does not support the installer.
+ */
+const downloadMessage = document.querySelector(".download-message")!;
+
+/**
+ * A boolean value which indicates if the user is on a `macOS` machine.
+ */
+const osIsMac = navigator.platform.toLowerCase().includes("mac"); // TODO Change navigator.platform to a non-deprecated value
+if (!osIsMac) {
+	downloadButton.classList.add("disabled");
+	downloadMessage.innerHTML = "The installer can only be run on <strong>macOS</strong>.";
+}
